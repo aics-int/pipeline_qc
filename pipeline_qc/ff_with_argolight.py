de278db41d7ee1e3ad85f7ae359d1aa65c78f08b
Unknown <calystay@alleninstitute.org>
2019-10-08 13:08:00 -0700

    Add 2d curve fit functions to extrapolate

--- pipeline_qc/ff_with_argolight.py
+++ pipeline_qc/ff_with_argolight.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from aicsimageio import AICSImage
-from scipy import interpolate
+from scipy import interpolate, optimize
 from scipy.optimize import curve_fit
 from skimage import filters, measure, io
 
 channel = '488'
 
+# Read images (flat field, black reference, argolight)
 ff_f_data = AICSImage(r'\\allen\aics\microscopy\PRODUCTION\OpticalControl\ZSD1_20190813\3500003331_100X_20190813_' + channel + '.czi')
 ff_f = ff_f_data.data[0, 0, 0, :, :]
 br_data = AICSImage(r'\\allen\aics\microscopy\PRODUCTION\OpticalControl\ZSD1_20190813\3500003331_100X_20190813_BR.czi')
@@ -14,19 +15,22 @@ br = br_data.data[0, 0, 0, :, :]
 argo_data = AICSImage(r'C:\Users\calystay\Desktop\argo_488_test.czi')
 argo = argo_data.data[0, 0, 0, : ,:]
 
+# Pre-process flat field images
 ff_smooth = filters.gaussian(image=ff_f, sigma=3, preserve_range=True)
 ff_norm = ff_smooth/np.max(ff_smooth)
 
-img_mask = np.zeros((ff_norm.shape), dtype=bool)
-for x in range (50, img_mask.shape[1], 200):
-    for y in range (25, img_mask.shape[0], 180):
-        img_mask[y:y+25, x:x+25] = True
-
+# Plot profiles for flat field images
 plot_profile(ff_f, px_crop=100, fit=False) # Intensity profile of raw dye ff image
 plot_profile(norm_img=ff_norm, px_crop=100, fit=False) # Intensity profile of normalized dye ff
 
 #=======================================================================================================================
 # Generate simulated homogeneity map with dye ff (sample across the image)
+
+# Sample a dye-ff image with points
+img_mask = np.zeros((ff_norm.shape), dtype=bool)
+for x in range (50, img_mask.shape[1], 100):
+    for y in range (50, img_mask.shape[0], 100):
+        img_mask[y:y+25, x:x+25] = True
 masked_ff = ff_smooth*img_mask
 label_ref = measure.label(img_mask)
 
@@ -58,7 +62,7 @@ compare_norm = compare/np.max(compare)
 plot_profile(compare_norm, 100, fit=False)
 
 # Option 2: From rings image, segment just the rings as samples
-argo_smooth = filters.gaussian(argo, sigma=3, preserve_range=True)
+argo_smooth = filters.gaussian(argo, sigma=3, preserve_range=True) # TODO: change filter, don't fill holes in argolight!
 thresh = filters.threshold_local(argo_smooth, block_size=11, offset=10)
 ff_argo_segment = (argo_smooth>thresh) & (argo_smooth>500)
 
@@ -81,10 +85,46 @@ norm_f = field_non_uni_raw/np.max(field_non_uni_raw)
 plot_profile(norm_f, px_crop=100) # Intensity profile of normalized simulated rings+cross ff
 
 #=======================================================================================================================
-# TODO: Curve fitting method does not extend to edge of image. Need to add functionality!!!
-params, cov = curve_fit(f=fit_2d_parabaloid, xdata=np.meshgrid(np.arange(624), np.arange(924)), ydata= )
-def fit_2d_parabaloid(x, a, b, c, d, e, f):
-    return a*(x-b)**2 + c*(y-d)**2 + e*x*y + f
+# 2D fit over (sampled) flat field images
+# Option 1: Fit the sampled image with a 2D parabaloid function
+sampled_img = masked_ff
+
+# Gather inputs for curve fit: x, y, z
+label_ref = measure.label(img_mask)
+field_non_uni_raw, z, coors = generate_homogeneity_ref(label_ref=label_ref, img_raw=ff_smooth, mode='median')
+x = []
+y = []
+for coor in coors:
+    x.append(coor[1])
+    y.append(coor[0])
+
+# Generate mesh grid to fit over
+all_x = np.arange(0, np.shape(sampled_img[1]))
+all_y = np.arange(0, np.shape(sampled_img[1]))
+xx, yy = np.meshgrid(all_x, all_y, sparse=True)
+
+# Fit (x,y), z to a 2d parabaloid function
+params_parabaloid, cov_parabaloid = curve_fit(f=fit_2d_parabaloid, xdata=(y, x), ydata=z)
+
+fit = fit_2d_parabaloid((yy, xx), *params_parabaloid)
+fit_field_non_uni_raw = fit.reshape(624, 924)
+plot_profile(fit_field_non_uni_raw, px_crop=0)
+
+# Option 2: Fit the normalized image (more data points) with a 2D gaussian function
+# 2D gaussian fit cannot find parameters for small sample size (tested with image with only 20 data points).
+# Only use 2D gaussian fit for more data points (e.g. normalized ff)
+
+params_gaussian = fitgaussian(ff_norm)
+fit_function = gaussian(*params_gaussian)
+fit_field_non_uni_raw = fit_function(*np.indices(ff_norm.shape))
+
+plot_profile(fit_field_non_uni_raw, px_crop=0)
+
+#=======================================================================================================================
+# Extract metrics from homogeneity map: roll-off %, center of hotspot, range of 10% intensity drop, px in bins
+
+homogeneity_map = ff_smooth # Select which method to use as homogeneity reference
+
 
 #=======================================================================================================================
 # Functions developed
@@ -136,12 +176,14 @@ def generate_homogeneity_ref(label_ref, img_raw, mode):
 
         coors.append((int(centroid[0]), int(centroid[1])))
     print (coors)
-    grid_0 = interpolate.griddata(points=coors, values=z, xi=all_coors, method='cubic', fill_value=False)
+    grid_0 = interpolate.griddata(points=coors, values=z, xi=all_coors, method='linear', fill_value=False)
+
     field_non_uni_raw = np.zeros((label_ref.shape))
     for x in range(0, len(grid_0)):
         point = all_coors[x]
         value = grid_0[x]
         field_non_uni_raw[point] = value
+
     return field_non_uni_raw, z, coors
 
 
@@ -159,3 +201,55 @@ def correct_img(img_to_corr, br, img_homogeneity_ref):
     return corr
 
 
+def fit_2d_parabaloid(xdata_tuple, a, b, c, d, e, f):
+    (y, x) = xdata_tuple
+    g = a*(x-b)**2 + c*(y-d)**2 + e*x*y + f
+    return g.ravel()
+
+
+def gaussian(height, center_x, center_y, width_x, width_y):
+    """
+
+    :param height: amplitude
+    :param center_x: xo
+    :param center_y: yo
+    :param width_x: x
+    :param width_y: y
+    :return: a gaussian function with the given parameters
+    """
+
+    width_x = float(width_x)
+    width_y = float(width_y)
+
+    return lambda x, y: height * np.exp(-(((center_x - x) / width_x) ** 2 + ((center_y - y) / width_y) ** 2) / 2)
+
+
+def moments(data):
+    """
+
+    :param data: image to fit over (e.g. ff_norm)
+    :return: the gaussian parameters of a 2D distribution by calculating its
+        moments (height, x, y, width_x, width_y)
+    """
+    total = data.sum()
+    X, Y = np.indices(data.shape)
+    x = (X * data).sum() / total
+    y = (Y * data).sum() / total
+    col = data[:, int(y)]
+    width_x = np.sqrt(np.abs((np.arange(col.size) - y) ** 2 * col).sum() / col.sum())
+    row = data[int(x), :]
+    width_y = np.sqrt(np.abs((np.arange(row.size) - x) ** 2 * row).sum() / row.sum())
+    height = data.max()
+    return height, x, y, width_x, width_y
+
+
+def fitgaussian(data):
+    """
+
+    :param data: image to fit over (e.g. ff_norm)
+    :return: gaussian parameters of a 2D distribution found by a fit (height, x, y, width_x, width_y)
+    """
+    params = moments(data)
+    errorfunction = lambda p: np.ravel(gaussian(*p)(*np.indices(data.shape)) - data)
+    p, success = optimize.leastsq(errorfunction, params)
+    return p
