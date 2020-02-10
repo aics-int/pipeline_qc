8b1a4ccc5cb136d05d78a445b3934b5bcfb2b0a7
Unknown <calystay@alleninstitute.org>
2020-02-10 14:05:05 -0800

    Update plot for ff

--- pipeline_qc/plot_ff.py
+++ pipeline_qc/plot_ff.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 
-plot_save_folder = r'\\allen\aics\microscopy\Calysta\test\optical_control_plots\ff\20191010'
+plot_save_folder = r'\\allen\aics\microscopy\Calysta\test\optical_control_plots\ff\20191121'
 
 df = pd.read_csv(r'C:\Users\calystay\Desktop\ff_output_20191010_2.csv', parse_dates=['date'])
 
@@ -66,4 +67,40 @@ for system in systems:
                                  grid=True)
             fig.savefig(os.path.join(plot_save_folder, title + '.png'))
             plt.close()
+
+
+metric = 'img_roll_off'
+
+zsd_colors = ["130122163", ""]
+
+for channel in channels:
+    df_plot = df.loc[(df['date'] > (year + '-01-01')) & (df['channel'] == channel)]
+    fig = plt.figure()
+    ax = sns.scatterplot(x="date", y="img_roll_off", data=df_plot, hue="system")
+    plt.xlim((df_plot['date'].min(), df_plot['date'].max()))
+    plt.ylim()
+    plt.title('intensity roll off in ' + str(channel))
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
