b9ee3e72d52d80a5916eb587771eaf9f9c5eb4d6
calystay <calystay@alleninstitute.org>
2020-04-14 13:07:14 -0700

    fix bugs

--- pipeline_qc/upload_align_h2b_to_fms.py
+++ pipeline_qc/upload_align_h2b_to_fms.py
@@ -1,29 +1,29 @@
 # Upload aligned H2B data to fms, with alignment info in metadata block
-
 from aicsfiles import FileManagementSystem
 from aicsfiles.filter import Filter
 import os
 import pandas as pd
 
-# Create an FMS connection (to stg-aics when testing!!)
-prod_fms = FileManagementSystem(host='aics')
+LK_ENV = 'stg-aics'
+INPUT_CSV = '/allen/aics/microscopy/Data/alignV2/align_info.csv'
+FOLDER = '/allen/aics/microscopy/Calysta/test/h2b_aligned'
+
+fms = FileManagementSystem(host=LK_ENV)
+df = pd.read_csv(INPUT_CSV)
 
 # alignment_reference
-df = pd.read_csv('/allen/aics/microscopy/Data/alignV2/align_info.csv')
 df['date'] = df['date'].astype(str)
 
 # String or Path object of file to be uploaded to FMS
-folder = '/allen/aics/microscopy/Calysta/test/h2b_aligned'
 failed_files = []
-all_files = os.listdir(failed_files)
+all_files = os.listdir(FOLDER)
 for file in all_files:
     if file.endswith('.tiff'):
-        #file = '3500002669_100X_20190118_1-alignV2-Scene-54-P55-C08.tiff'
-        new_file_path = os.path.join(folder, file)
+        new_file_path = os.path.join(FOLDER, file)
 
         # Find the file we want to copy the initial metadata blob from
         content_proc_filter = Filter().with_file_name(file.replace('-alignV2', '').replace('.tiff', '.czi'))
-        result = prod_fms.query_files(content_proc_filter)
+        result = fms.query_files(content_proc_filter)
 
         # get instrument and date from file
         file_path = result[0]['file']['original_path']
@@ -47,8 +47,8 @@ for file in all_files:
                 new_metadata['content_processing']['two_camera_alignment']['shift_y'] = shift_y
                 new_metadata['content_processing']['two_camera_alignment']['scaling'] = scaling
                 new_metadata['content_processing']['two_camera_alignment']['rotation_angle'] = rotation_angle
-                prod_fms.upload_file(new_file_path, new_metadata)
+                fms.upload_file(new_file_path, new_metadata)
         else:
             failed_files.append(file)
 
-pd.DataFrame(failed_files).to_csv(os.path.join(folder, 'fail_upload.csv'))
+pd.DataFrame(failed_files).to_csv(os.path.join(FOLDER, 'fail_upload.csv'))
