bf87ebbc396501725dcd403a99a2bd55f55be496
Aditya Nath <aditya.nath@alleninstitute.org>
2020-06-18 16:27:20 -0700

    Fix to handle error where FOV has no cells

--- pipeline_qc/image_qc_methods/intensity.py
+++ pipeline_qc/image_qc_methods/intensity.py
@@ -8,6 +8,17 @@ def intensity_stats_single_channel(single_channel_im, cell_mask):
 
     single_channel_im_seg = single_channel_im * cell_mask
     result = dict()
+
+    # Handles empty FOV case (where no cells are in the FOV)
+    if single_channel_im_seg[np.nonzero(single_channel_im_seg)].size == 0:
+        result.update({'mean': 0})
+        result.update({'median': 0})
+        result.update({'max': 0})
+        result.update({'min': 0})
+        result.update({'std': 0})
+        result.update({'99.5%': 0})
+        result.update({'0.5%': 0})
+
     result.update({'mean': single_channel_im_seg[np.nonzero(single_channel_im_seg)].mean()})
     result.update({'median': np.median(single_channel_im_seg[np.nonzero(single_channel_im_seg)])})
     result.update({'max': single_channel_im_seg[np.nonzero(single_channel_im_seg)].max()})
