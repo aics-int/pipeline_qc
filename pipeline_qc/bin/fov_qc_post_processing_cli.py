cca604ff8471fb1713168907e90958b1a26a3f03
Aditya Nath <aditya.nath@alleninstitute.org>
2020-08-13 16:49:03 -0700

    Pass/fail criteria generation for fov qc metrics
    Post processing script after fov qc metrics are generated, and cli for running of the script

---
+++ pipeline_qc/bin/fov_qc_post_processing_cli.py
@@ -0,0 +1,77 @@
+"""
+This sample script will get deployed in the bin directory of the
+users' virtualenv when the parent module is installed using pip.
+"""
+
+import argparse
+import logging
+import sys
+import traceback
+
+from pipeline_qc.image_qc_methods import fov_qc_post_processing
+
+###############################################################################
+
+log = logging.getLogger()
+# Note: basicConfig should only be called in bin scripts (CLIs).
+# https://docs.python.org/3/library/logging.html#logging.basicConfig
+# "This function does nothing if the root logger already has handlers configured for it."
+# As such, it should only be called once, and at the highest level (the CLIs in this case).
+# It should NEVER be called in library code!
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s - %(name)s - %(lineno)3d][%(levelname)s] %(message)s')
+
+###############################################################################
+
+
+class Args(argparse.Namespace):
+
+    def __init__(self):
+        super().__init__()
+        # Arguments that could be passed in through the command line
+        self.env = 'stg'
+        #
+        self.__parse()
+
+    def __parse(self):
+        p = argparse.ArgumentParser(prog='FOV QC Post Processing',
+                                    description='Generates pass fail criteria from FOV QC metrics')
+        p.add_argument('--env', type=str,
+                       help="Environment that data will be stored to('prod, 'stg', default is 'stg')",
+                       default='stg', required=False)
+        p.add_argument('--debug',
+                       help='Enable debug mode',
+                       default=False, required=False, action='store_true')
+
+        p.parse_args(namespace=self)
+
+
+###############################################################################
+
+def main():
+    args = Args()
+    dbg = args.debug
+
+    try:
+        # Do your work here - preferably in a class or function,
+        # passing in your args. E.g.
+        fov_qc_post_processing.update_qc_data_labkey(
+            df=fov_qc_post_processing.z_score_stat_generation(),
+            env = args.env,
+        )
+
+    except Exception as e:
+        log.error("=============================================")
+        if dbg:
+            log.error("\n\n" + traceback.format_exc())
+            log.error("=============================================")
+        log.error("\n\n" + str(e) + "\n")
+        log.error("=============================================")
+        sys.exit(1)
+
+
+###############################################################################
+# Allow caller to directly run this module (usually in development scenarios)
+
+if __name__ == '__main__':
+    main()
